514b217bbcad0645f624f05a2ab812ebe70bf560
Marcos L <marcosrlj@outlook.com>
2021-03-03 04:40:51 -0300

    added database
    configured typeorm
    created users route
    created users controller
    created users migration
    created users model
    added logging on ormconfig

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
+import 'reflect-metadata'
 import express from 'express'
+import { router } from './routes'
+
+import './database'
+
+const PORT = 3333
 
 const app = express()
 
-const PORT = 3333
-app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)})
+app.use(express.json())
+
+app.use(router)
+
+app.listen(PORT, () => {console.log(`Listening on  http://localhost:${PORT}/`)})
